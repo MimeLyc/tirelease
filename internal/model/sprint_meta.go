5830fd7127496c3a8d1a3d40b27364029f381d3d
李煜超 <yuchao.li@pingcap.com>
2022-09-08 22:32:18 +0800

    refactor(sprint)

--- internal/model/sprint_meta.go
+++ internal/model/sprint_meta.go
@@ -1,6 +1,10 @@
 package model
 
-import "tirelease/internal/entity"
+import (
+	"time"
+	"tirelease/internal/entity"
+	"tirelease/internal/repository"
+)
 
 type SprintMeta struct {
 	entity.SprintMeta
@@ -14,4 +18,42 @@ func (sprint SprintMeta) GetMajorVersion() int {
 func (sprint SprintMeta) GetMinorVersion() int {
 	_, minor, _, _ := ComposeVersionAtom(sprint.MinorVersionName)
 	return minor
+}
+
+// StartTime of a sprint if the checkout time of last sprint.
+func (sprint SprintMeta) GetStartTime() (*time.Time, error) {
+	major := sprint.Major
+	minor := sprint.Minor
+	lastSprint, err := SelectLastSprint(major, minor, sprint.Repo)
+	lastMinorVersionName := ""
+
+	// If there is data of last Sprint, just return the stored value
+	if lastSprint != nil && lastSprint.CheckoutCommitTime != nil {
+		return lastSprint.CheckoutCommitTime, nil
+	} else if minor > 0 {
+		lastMinorVersionName = ComposeVersionMinorNameByNumber(major, minor-1)
+	} else {
+		return nil, err
+	}
+
+	return GetCheckoutTimeOfSprint(sprint.Repo.Owner, sprint.Repo.Repo, lastMinorVersionName)
+}
+
+func (sprint SprintMeta) GetCheckoutTime() (*time.Time, error) {
+	major := sprint.Major
+	minor := sprint.Minor
+	sprintEntity, _ := repository.SelectSprintMetaUnique(
+		&entity.SprintMetaOption{
+			Major: &major,
+			Minor: &minor,
+			Repo:  &sprint.Repo,
+		},
+	)
+
+	// If there is data of last Sprint, just return the stored value
+	if sprintEntity != nil && sprintEntity.CheckoutCommitTime != nil {
+		return sprintEntity.CheckoutCommitTime, nil
+	} else {
+		return GetCheckoutTimeOfSprint(sprint.Repo.Owner, sprint.Repo.Repo, ComposeVersionMinorNameByNumber(major, minor))
+	}
 }
--- internal/model/sprint_meta_helper.go
+++ internal/model/sprint_meta_helper.go
@@ -8,45 +8,41 @@ import (
 	"tirelease/internal/repository"
 )
 
-func (sprint SprintMeta) GetStartTime() (*time.Time, error) {
-	major := sprint.Major
-	minor := sprint.Minor
-	lastSprint, err := SelectLastSprint(major, minor, sprint.Repo)
-	lastMinorVersionName := ""
-
-	// If there is data of last Sprint, just return the stored value
-	if lastSprint != nil && lastSprint.CheckoutCommitTime != nil {
-		return lastSprint.CheckoutCommitTime, nil
-	} else if minor > 0 {
-		lastMinorVersionName = ComposeVersionMinorNameByNumber(major, minor-1)
-	} else {
+// Format of sprint name: x.x
+func GetCheckoutTimeOfSprint(owner, repo, sprintName string) (*time.Time, error) {
+	commit, err := GetCheckoutCommit(owner, repo, sprintName)
+	if commit == nil {
 		return nil, err
 	}
+	return &commit.CommittedTime, nil
+}
 
+func GetCheckoutCommit(owner, repo, sprintName string) (*GitCommit, error) {
 	// If there is tag named vx.x.0, then trace back the checkout commit from it
 	// because it's the nearest commit of checkout commit
-	firstTagOfLastSprint := fmt.Sprintf("v%s.0", lastMinorVersionName)
-	checkoutCommit, err := GetCheckoutCommitOfRef(sprint.Repo.Owner, sprint.Repo.Repo, firstTagOfLastSprint, git.RefTypeTag)
+	firstTagOfLastSprint := fmt.Sprintf("v%s.0", sprintName)
+	checkoutCommit, err := GetCheckoutCommitOfRef(owner, repo, firstTagOfLastSprint, git.RefTypeTag)
 	if err != nil {
 		return nil, err
 	}
 	if checkoutCommit != nil {
-		return &checkoutCommit.CommittedTime, nil
+		return checkoutCommit, nil
 	}
 
 	// If there is **not** tag named vx.x.0, trace back the checkout commit from version branch "release-x.x"
 	// because there is no tag on the branch
-	branchOfLastSprint := fmt.Sprintf("%s%s", git.ReleaseBranchPrefix, lastMinorVersionName)
-	checkoutCommit, err = GetCheckoutCommitOfRef(sprint.Repo.Owner, sprint.Repo.Repo, branchOfLastSprint, git.RefTypeBranch)
+	branchOfLastSprint := fmt.Sprintf("%s%s", git.ReleaseBranchPrefix, sprintName)
+	checkoutCommit, err = GetCheckoutCommitOfRef(owner, repo, branchOfLastSprint, git.RefTypeBranch)
 	if err != nil {
 		return nil, err
 	}
 	if checkoutCommit != nil {
-		return &checkoutCommit.CommittedTime, nil
+		return checkoutCommit, nil
 	}
 
 	// If the checkout commit is not found and the err is nil, the branch of last sprint is not checkouted.
-	return nil, fmt.Errorf("Last branch of sprint %s has not been checkouted.", lastMinorVersionName)
+	return nil, fmt.Errorf("Last branch of sprint %s has not been checkouted.", sprintName)
+
 }
 
 func SelectLastSprint(major, minor int, repo entity.Repo) (*entity.SprintMeta, error) {
