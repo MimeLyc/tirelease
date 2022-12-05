17ac3e963e1981118f11141dd4186d019a9e3051
李煜超 <yuchao.li@pingcap.com>
2022-12-05 16:34:01 +0800

    fix(release note): remove redundant function

--- internal/model/issue_pr_relation.go
+++ internal/model/issue_pr_relation.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"tirelease/commons/git"
 	"tirelease/internal/entity"
 	"tirelease/internal/repository"
 )
@@ -16,63 +15,6 @@ type IssuePrRelation struct {
 type PrIssueRelation struct {
 	*PullRequest
 	RelatedIssues []Issue
-}
-
-func GetIssuePrRelations(major, minor int, option entity.IssueOption) ([]IssuePrRelation, error) {
-	versionName := ComposeVersionMinorNameByNumber(major, minor)
-	branchName := git.ReleaseBranchPrefix + versionName
-	affects, err := repository.SelectIssueAffect(
-		&entity.IssueAffectOption{
-			AffectVersion: versionName,
-			AffectResult:  entity.AffectResultResultYes,
-		},
-	)
-	if err != nil || len(*affects) == 0 {
-		return nil, err
-	}
-
-	issueIds := ExtractIssueIDs(*affects)
-	option.IssueIDs = issueIds
-	issues, err := repository.SelectIssue(&option)
-	if err != nil {
-		return nil, err
-	}
-	issuePrRelations, err := repository.SelectIssuePrRelation(
-		&entity.IssuePrRelationOption{
-			IssueIDs: issueIds,
-		},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	result := make([]IssuePrRelation, 0)
-
-	for _, issue := range *issues {
-		issue := issue
-		prids := ExtractPrIdsByIssueId(*issuePrRelations, issue.IssueID)
-		if len(prids) == 0 {
-			continue
-		}
-		prs, err := repository.SelectPullRequest(
-			&entity.PullRequestOption{
-				BaseBranch:     branchName,
-				PullRequestIDs: prids,
-			},
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		result = append(result, IssuePrRelation{
-			Major:      major,
-			Minor:      minor,
-			Issue:      &issue,
-			RelatedPrs: *prs,
-		})
-	}
-
-	return result, nil
 }
 
 type PrIssueRelationBuilder struct {
